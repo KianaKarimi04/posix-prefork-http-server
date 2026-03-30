4509faf4315ef3eae1228514fb52e1c9699a37d0
kianakarimi04 <kianakarimi8383@gmail.com>
2026-03-29 22:54:05 -0700

    Implemented handler.h

---
+++ handler.h
@@ -0,0 +1,10 @@
+//
+// Created by kiana on 3/29/26.
+//
+
+#ifndef POSIX_PREFORK_HTTP_SERVER1_HANDLER_H
+#define POSIX_PREFORK_HTTP_SERVER1_HANDLER_H
+
+void handle_client(int client);
+
+#endif //POSIX_PREFORK_HTTP_SERVER1_HANDLER_H
