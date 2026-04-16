5388a6c77e30281e6359b8a3251ad5ab16eed7a9
kianakarimi04 <kianakarimi8383@gmail.com>
2026-04-15 23:34:42 -0700

    fix the db

--- db_query.c
+++ db_query.c
@@ -1,29 +1,27 @@
 //
 // Created by kiana on 3/29/26.
 //
-#include <stdio.h>
-#include <ndbm.h>
 #include <fcntl.h>
+#include <ndbm.h>
+#include <stdio.h>
 
 int main() {
-    DBM *db = dbm_open("data.db", O_RDONLY, 0666);
+  DBM *db = dbm_open("data.db", O_RDONLY, 0666);
 
-    if (!db) {
-        printf("Database not found or empty.\n");
-        return 1;
-    }
+  if (!db) {
+    printf("Database not found or empty.\n");
+    return 1;
+  }
 
-    datum key = dbm_firstkey(db);
+  datum key = dbm_firstkey(db);
 
-    while (key.dptr) {
-        datum value = dbm_fetch(db, key);
+  while (key.dptr) {
+    datum value = dbm_fetch(db, key);
 
-        printf("%.*s => %.*s\n",
-            key.dsize, key.dptr,
-            value.dsize, value.dptr);
+    printf("%.*s => %.*s\n", key.dsize, key.dptr, value.dsize, value.dptr);
 
-        key = dbm_nextkey(db);
-    }
+    key = dbm_nextkey(db);
+  }
 
-    dbm_close(db);
+  dbm_close(db);
 }
